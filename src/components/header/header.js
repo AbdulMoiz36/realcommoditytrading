3aaf00b6c0d6132437ef211b8c268e136ad967e0
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-04-12 18:31:12 +0500

    Responsive Navbar Without Dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,7 +6,7 @@ import Navbar from "./navbar";
 const Header = () => {
   return (
     <>
-      <div className="bg-blue-700 text-white h-7">
+      <div className="bg-blue-600 text-white h-7">
         <marquee>
           {" "}
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero fugit
@@ -16,12 +16,12 @@ const Header = () => {
         </marquee>
       </div>
 
-      <div className="bg-green-600 text-white grid gap-5 p-2 px-[5%] grid-cols-2 md:grid-cols-3">
-  <div className="order-1 mx-auto text-center flex items-center">
-    <p className="font-bold quick-guide">Quick Guide</p>
+      <div className="bg-green-700 text-white grid gap-3 p-2 px-[5%] grid-cols-1 md:grid-cols-3">
+  <div className=" mx-auto text-center flex items-center">
+    <p className="font-bold text-lg quick-guide">Quick Guide</p>
   </div>
 
-  <div className="order-3 md:order-2 col-span-full md:col-span-1 ">
+  <div className="col-span-full md:col-span-1 ">
     <input
       type="text"
       placeholder="Search..."
@@ -29,16 +29,15 @@ const Header = () => {
     />
   </div>
 
-  <div className="order-2 md:order-3 mx-auto text-center md:flex md:items-center md:gap-3">
+  <div className="mx-auto text-center grid grid-cols-2 place-items-center">
     <Link to="/login" className="" >
-      <p className="font-medium">Login/Register</p>
+      <p className="font-medium ">Login/Register</p>
     </Link>
-    <select name="" id="" className="px-6 py-1 bg-transparent outline-none border font-medium mt-2 md:mt-0 text-white">
-      <option value="English" className="text-black " selected> English</option>
+    <select name="" id="" defaultValue="English" className="px-6 py-1 bg-transparent outline-none border font-medium mt-2 md:mt-0 text-white">
+      <option value="English" className="text-black "> English</option>
     </select>
   </div>
 </div>
-
       <Navbar />
     </>
   );
--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMdMenu, IoIosClose , IoMdArrowDropdown } from 'react-icons/io';
 
 const Navbar = () => {
-  return <nav className="">
-    
-  </nav>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIcon = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="flex justify-between items-center w-full mx-auto p-2 px-10 md:px-32 lg:px-16 ">
+      <div className="">
+        <img src="https://realcommoditytrading.com/frontend/new_assets/img/logo.svg" alt="Logo" className="w-auto" />
+      </div>
+
+      <div className={`nav-links lg:static  lg:min-h-fit absolute shadow shadow-black lg:shadow-transparent bg-white flex flex-col  gap-5 min-h-screen ${isOpen ? 'left-0 transition-all duration-300' : 'left-[-100%] transition-all duration-300'} top-0 lg:w-auto md:w-[50%] w-[75%] flex items-start`}>
+        <div className="bg-green-700 p-8 flex items-center justify-center w-full lg:hidden">
+          <img src="https://realcommoditytrading.com/frontend/new_assets/img/logo.svg" alt="Logo" className="w-auto h-auto" />
+          <IoIosClose onClick={toggleIcon} className="text-3xl cursor-pointer lg:hidden absolute top-0 right-0 text-white" />
+        </div>
+        <ul className="flex lg:flex-row flex-col lg:items-center gap-5 lg:gap-[2vw] px-8">
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw]" href="#">Home</a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw]" href="#">About Us</a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">PETROLEUM<IoMdArrowDropdown /></a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">METAL<IoMdArrowDropdown /></a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">AGRI/MEAT<IoMdArrowDropdown /></a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">FINANCE<IoMdArrowDropdown /></a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">SERVICE<IoMdArrowDropdown /></a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">Q&A<IoMdArrowDropdown /></a>
+          </li>
+          <li>
+            <a className="hover:text-green-500 font-semibold lg:text-[1.1vw] flex items-center gap-1" href="#">Inquiry<IoMdArrowDropdown /></a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex items-center gap-6 lg:hidden">
+        {isOpen ? (
+          <IoIosClose onClick={toggleIcon} className="text-3xl cursor-pointer lg:hidden" />
+        ) : (
+          <IoMdMenu onClick={toggleIcon} className="text-3xl cursor-pointer lg:hidden" />
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
