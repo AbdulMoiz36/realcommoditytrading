794f8e9ad8f1c0dee842f831891d0fc98fd884e2
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-04-14 13:22:58 +0500

    verified offer section (Home)

---
+++ src/components/card.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BiMessageDetail } from "react-icons/bi";
+import { IoMdHeart, IoMdEye  } from "react-icons/io";
+import { Link } from "react-router-dom";
+import Button from "./button";
+
+const Card = ({ title, category, likes, views, comments, date }) => {
+  return (
+    <>
+      <div className="card-div rounded-xl p-5 mx-3 md:mx-8 my-6 h-96 flex flex-col justify-between shadow-lg shadow-neutral-300 border-amber-400 border-2 ">
+        <div className= "flex justify-between">
+            <p className="underline font-medium text-gray-600 p-2 text-sm">{date}</p>
+            <p className="text-white bg-red-600 py-2 px-7 rounded-md text-sm self-center">{category}</p>
+        </div>
+        <h3 className=" my-4 font-bold text-2xl overflow-auto">{title}</h3>
+        <div className="flex lg:justify-around justify-between gap-1 border-b-4 p-4 border-t-4 border-gray-500 rounded-md">
+          <div>
+            <Button text={"Read More"} color={"blue"} link={"#"}/>
+          </div>
+          <div className="flex gap-4">
+            <div className="flex flex-col justify-center items-center text-center">
+              <IoMdHeart className="text-red-600"/>
+              <p>{likes}</p>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center">
+            <BiMessageDetail />
+              <p>{comments}</p>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center">
+            <IoMdEye className="text-blue-600"/>
+              <p>{views}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,17 +1,156 @@
 import React from "react";
 import Button from "../../components/button";
-
+import Card from "../../components/card";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 const home = () => {
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block"}}
+        onClick={onClick}
+      />
+    );
+  }
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 800,
+    slidesToShow: 3,
+    slidesToScroll: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1100,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
     <>
-     <div className="banner h-[80vh] w-full flex justify-center items-center">
-  <div className="text-white flex flex-col justify-center items-start mx-8 md:mx-28 lg:pr-[30%] gap-8 ">
-    <h2 className="font-bold text-4xl md:text-6xl ">Global Partners Network for Commodity Trading Business</h2>
-    <h4 className="font-semibold text-lg">We help finding qualified RWA sellers and buyers through our network of partners for companies who request us with Previous Records, Proof of Products or Funds.</h4>
-    <Button text="Join Us As A Partner" color="yellow" link="/example" /> 
-  </div>
-</div>
+      <div className="banner h-[80vh] w-full flex justify-center items-center">
+        <div className="text-white flex flex-col justify-center items-start mx-8 md:mx-28 lg:pr-[30%] gap-8 ">
+          <h2 className="font-bold text-4xl md:text-6xl ">
+            Global Partners Network for Commodity Trading Business
+          </h2>
+          <h4 className="font-semibold text-lg">
+            We help finding qualified RWA sellers and buyers through our network
+            of partners for companies who request us with Previous Records,
+            Proof of Products or Funds.
+          </h4>
+          <Button text="Join Us As A Partner" color="yellow" link="/#" />
+          <p className="font-bold quick-guide">
+            Win $100,00 <a href="#"> Find More &gt;</a>
+          </p>
+        </div>
+      </div>
+      <div className="w-full flex flex-col  mx-auto ">
+          <div className="bg-blue-500 w-4/5 md:w-3/5 text-center rounded-t-3xl mt-14 p-6 mx-auto">
+            <h2 className="font-bold text-5xl text-white">Verified Offers</h2>
+          </div>
+          <div className=" w-4/5 md:w-3/5 mx-auto text-center p-7 border-x-2 border-blue-500 bg-white">
+            <div>
+              <p className="mb-7">
+          Verified offers are created by our company only if Previous Record(s), Proof of Product(s), or Proof of Fund(s) is provided to us.
+              </p>
+           {/* <Button text={"Request a Verified Offer Post"} color={"yellow"} link={"#"}></Button>       */}
+            </div>
+          </div>
+      </div>
+      <div className=" px-10 md:mx-12 lg:mx-32 border-y-4 border-blue-500 rounded-lg mt-[-0.5rem] z-10 ">
+      <Slider {...settings} >
+        <Card
+          title="Article Title"
+          category="Technology"
+          likes={25}
+          views={100}
+          comments={5}
+          date="April 12, 2024"
+        />
+        <Card
+          title="In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content."
+          category="Technology"
+          likes={25}
+          views={100}
+          comments={5}
+          date="April 12, 2024"
+        />
+        <Card
+          title="Article Title"
+          category="Technology"
+          likes={25}
+          views={100}
+          comments={5}
+          date="April 12, 2024"
+        />
+        <Card
+          title="Article Title"
+          category="Technology"
+          likes={25}
+          views={100}
+          comments={5}
+          date="April 12, 2024"
+        />
+        <Card
+          title="Article Title"
+          category="Technology"
+          likes={25}
+          views={100}
+          comments={5}
+          date="April 12, 2024"
+        />
+        <Card
+          title="Article Title"
+          category="Technology"
+          likes={25}
+          views={100}
+          comments={5}
+          date="April 12, 2024"
+        />
+        </Slider>
+        <div className="text-center mt-10 mb-5">
+        <Button text={"Request a Verified Offer Post"} color={"yellow"} link={"#"}></Button>
+        </div>
+      </div>
+      <div className="p-32">
 
+      </div>
     </>
   );
 };
