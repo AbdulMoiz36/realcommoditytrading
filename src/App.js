11e3c0550bfc18406f79119463a0cad632fed43d
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-04-16 16:31:47 +0500

    half designing of signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import Home from './pages/home/home';
 import { Footer } from './components/footer/footer';
+import Login from './pages/auth/login';
+import Signup from './pages/auth/signup';
 const App = () => {
   return (
     <Router>
@@ -10,6 +12,8 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
         <Footer/>
       </div>
